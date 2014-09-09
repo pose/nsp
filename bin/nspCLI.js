56e2fc7b17c692ef3a2804be5b3d58c8d2bd359e
tomsteele <thomasjsteele@gmail.com>
2014-09-09 09:10:16 -0700

    Adds error message for unknown commands

--- bin/nspCLI.js
+++ bin/nspCLI.js
@@ -1,6 +1,32 @@
 #!/usr/bin/env node
 
 var celeri = require('celeri');
+var speller = require('speller');
 require('./../commands');
 
-celeri.parse(process.argv);
+var commands = ['package', 'audit-package', 'shrinkwrap', 'audit-shrinkwrap'];
+
+celeri.parse(process.argv, function (err, cmd) {
+
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    var command = cmd.command[0];
+
+    if (commands.indexOf(command) === -1) {
+        speller.train(commands.join(' '));
+        var corrected = speller.correct(command);
+
+        if (corrected === command) {
+            console.error('Unknown command "' + command + '". Did you mean:');
+            for (var i=0; i < commands.length; i++) {
+                console.error('- ' + commands[i]);
+            }
+        } else {
+            console.error('Unknown command "' + command + '". Did you mean "' + corrected + '"?');
+        }
+     }
+
+});
